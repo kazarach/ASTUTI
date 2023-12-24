cf32e9db2da0f4cda2527345ae29fe28283e2e70
kazarach <kazarachmain@gmail.com>
2023-12-25 03:06:06 +0700

    Update main send.cpp

--- test/main send.cpp
+++ test/main send.cpp
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <TaskScheduler.h>
+
+Scheduler taskScheduler;
 
 const int trigPin = D1;  // D1 (GPIO5) on ESP8266
 const int echoPin = D2;  // D2 (GPIO4) on ESP8266
@@ -25,6 +28,28 @@ void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   }
 }
 
+void readSensor(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  unsigned long duration = pulseIn(echoPin, HIGH);
+  sensorData.servo_moved = false;
+
+  sensorData.distance_cm = duration / 58.2;
+
+  esp_now_send(broadcastAddress, (uint8_t *)&sensorData, sizeof(sensorData));
+
+  // Menampilkan informasi ke Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(sensorData.distance_cm);
+  Serial.println(" cm");
+}
+
+Task taskSensor(5000, TASK_FOREVER, &readSensor);
+
 void setup() {
   Serial.begin(115200);
 
@@ -42,24 +67,16 @@ void setup() {
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+
+  taskScheduler.init();
+  taskScheduler.addTask(taskSensor); // Menambahkan objek Task ke scheduler
+  taskSensor.enable();
+  taskScheduler.startNow(); 
+  readSensor(); // Membaca sensor pertama kali saat inisialisasi
 }
 
 void loop() {
-  if ((millis() - lastTime) > timerDelay) {
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-    unsigned long duration = pulseIn(echoPin, HIGH);
-    sensorData.servo_moved = false;
-
-    sensorData.distance_cm = duration / 58.2;
-
-    esp_now_send(broadcastAddress, (uint8_t *)&sensorData, sizeof(sensorData));
-
-    lastTime = millis();
-    // timerDelay = 5000;
-  }
+  taskScheduler.execute();
+  // taskSensor.execute();
+  // readSensor();
 }
